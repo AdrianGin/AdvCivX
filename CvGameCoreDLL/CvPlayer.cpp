AdrianGin/AdvCivX

commit fd6760d3a88afbc5469b765f6ccc1909ed473a6a

Fix auto-moves not getting triggered by unit cycle wrap
Amends commit
13a766ce5e318d443dbe3a510cfdd6c3a7038bf9
[advc.154]

Bug report:
https://forums.civfanatics.com/threads/advanced-civ.614217/post-16398985

CvGameCoreDLL/CvPlayer.cpp
@@ -2348,12 +2348,22 @@ void CvPlayer::killUnits()
 
 // advc.154: Cut from cycleSelectionGroups except for the non-const parts
 CvSelectionGroup* CvPlayer::getNextGroupInCycle(CvUnit* pUnit, bool bForward,
-	bool bWorkers, bool* pbWrap) const
+	bool bWorkers, bool* pbWrap,
+	std::set<int>* pCycledGroups) const
 {
 	FAssert(isActive() && isHuman());
 	LOCAL_REF(bool, bWrap, pbWrap, false); // K-Mod
-	// advc.154: Copy the set
-	std::set<int> kCycledGroups = GC.getGame().getActivePlayerCycledGroups(); // K-Mod
+	//std::set<int>& kCycledGroups = GC.getGame().getActivePlayerCycledGroups(); // K-Mod
+	// <advc.154>
+	std::set<int> kCycledGroupsCopy;
+	if (pCycledGroups == NULL)
+	{
+		// Don't want to mark groups as cycled; therefore make a copy.
+		kCycledGroupsCopy = GC.getGame().getActivePlayerCycledGroups();
+	}
+	std::set<int>& kCycledGroups = (pCycledGroups == NULL ?
+			kCycledGroupsCopy : *pCycledGroups);
+	// </advc.154>
 	CLLNode<int>* pSelectionGroupNode = headGroupCycleNode();
 	if (pUnit != NULL)
 	{
@@ -2459,9 +2469,10 @@ CvSelectionGroup* CvPlayer::cycleSelectionGroups(CvUnit* pUnit, bool bForward,
 			pSelectionGroupNode = nextGroupCycleNode(pSelectionGroupNode);
 		}
 	}
-	// advc.154: Moved into new const function
-	CvSelectionGroup* pGroup = getNextGroupInCycle(pUnit, bForward, bWorkers, pbWrap);
-	if (pbWrap != NULL && pbWrap)
+	// <advc.154> Moved into new const function
+	CvSelectionGroup* pGroup = getNextGroupInCycle(pUnit, bForward,
+			bWorkers, pbWrap, &kCycledGroups); // </advc.154>
+	if (pbWrap != NULL && *pbWrap)
 		kCycledGroups.clear(); // </K-Mod>
 	return pGroup;
 }

CvGameCoreDLL/CvGame.h
@@ -98,7 +98,7 @@ public:
 	DllExport void cycleCities(bool bForward = true, bool bAdd = false) const;							// Exposed to Python
 	// <advc.154>
 	CvSelectionGroup* getNextGroupInCycle(bool bForward, bool bWorkers,
-			bool& bWrap, CvUnit*& pCycleUnit) const;
+			bool& bWrap, CvUnit*& pCycleUnit, std::set<int>* pCycledGroups = NULL) const;
 	CvUnit* getCycleButtonUnit(bool bForward, bool bWorkers) const; // </advc.154>						// Exposed to Python
 	void cycleSelectionGroups(bool bClear,																// Exposed to Python
 			bool bForward = true, bool bWorkers = false);

CvGameCoreDLL/CvGameInterface.cpp
@@ -562,7 +562,8 @@ void CvGame::cycleCities(bool bForward, bool bAdd) const
 
 // advc.154: Extracted the const part out of cycleSelectionGroups
 CvSelectionGroup* CvGame::getNextGroupInCycle(bool bForward, bool bWorkers,
-	bool& bWrap, CvUnit*& pCycleUnit) const // out-params
+	bool& bWrap, CvUnit*& pCycleUnit, // out-params
+	std::set<int>* pCycledGroups) const
 {
 	bWrap = false;
 	pCycleUnit = gDLL->UI().getHeadSelectedUnit();
@@ -572,7 +573,7 @@ CvSelectionGroup* CvGame::getNextGroupInCycle(bool bForward, bool bWorkers,
 		if (pCycleUnit->getOwner() != getActivePlayer())
 			pCycleUnit = NULL;
 		pGroup = GET_PLAYER(getActivePlayer()).getNextGroupInCycle(
-				pCycleUnit, bForward, bWorkers, &bWrap);
+				pCycleUnit, bForward, bWorkers, &bWrap, pCycledGroups);
 	}
 	else
 	{
@@ -624,9 +625,9 @@ void CvGame::cycleSelectionGroups(bool bClear, bool bForward, bool bWorkers)
 {
 	bool bWrap=false;
 	CvUnit* pCycleUnit=NULL;
-	// advc.154: Moved into a const function
+	// <advc.154> Moved into a const function ...
 	CvSelectionGroup* pNextSelectionGroup = getNextGroupInCycle(
-			bForward, bWorkers, bWrap, pCycleUnit);
+			bForward, bWorkers, bWrap, pCycleUnit); // </advc.154>
 	if (gDLL->UI().getHeadSelectedUnit() != NULL)
 	{
 		GET_PLAYER(getActivePlayer()).cycleSelectionGroups(

CvGameCoreDLL/CvPlayer.h
@@ -103,7 +103,8 @@ public:
 	void killUnits();																								// Exposed to Python
 	// <advc.154>
 	CvSelectionGroup* getNextGroupInCycle(CvUnit* pUnit, bool bForward,
-			bool bWorkers, bool* pbWrap) const; // </advc.154>
+			bool bWorkers, bool* pbWrap,
+			std::set<int>* pCycledGroup = NULL) const; // </advc.154>
 	CvSelectionGroup* cycleSelectionGroups(CvUnit* pUnit, bool bForward,
 			bool bWorkers, bool* pbWrap);
 

commit a74ace261041f40dcf066c402b153f1e5cacce5e

Amend unit cycle wrap behavior
Unit cycling had re-selected the same unit once at the end of the cycle. I don't fully understand the code; some quick tests suggest that it works correctly now, hopefully without introducing new problems. This latest issue was probably introduced by commit
fd6760d3a88afbc5469b765f6ccc1909ed473a6a. [advc.154]

CvGameCoreDLL/CvPlayer.cpp
@@ -2400,7 +2400,8 @@ CvSelectionGroup* CvPlayer::getNextGroupInCycle(CvUnit* pUnit, bool bForward,
 		CvSelectionGroup* pLoopSelectionGroup = getSelectionGroup(
 				pSelectionGroupNode->m_data);
 		if (pLoopSelectionGroup->readyToSelect(/* advc.153: */ true) &&
-			kCycledGroups.count(pSelectionGroupNode->m_data) <= 0) // K-Mod
+			(kCycledGroups.count(pSelectionGroupNode->m_data) <= 0 || // K-Mod
+			bWrap)) // advc.154: Don't want to return NULL upon wrapping
 		{
 			if (!bWorkers ||
 				// advc.153: with moves
@@ -2432,7 +2433,7 @@ CvSelectionGroup* CvPlayer::getNextGroupInCycle(CvUnit* pUnit, bool bForward,
 		if (pSelectionGroupNode == pFirstSelectionGroupNode)
 		{
 			// break; /* <K-Mod> */
-			if (bWrap)
+			if (bWrap) // (advc.154: Maybe can't occur anymore)
 				break;
 			bWrap = true; // </K-Mod>
 		}