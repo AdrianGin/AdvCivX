f1rpo/MMod

commit 53fb260a7892f0ac05fb72fd8d5db87e3670aa6b

Merge BULL pre-chop options
Originally by EmperorFool. Frequently (or, more so, urgently) requested by K-Mod players. Not safe for network games, so I'm letting the DLL treat pre-chop as disabled in network games. I've added a note about that to the hover text on the BUG menu.

The AdvCiv implementation of pre-chop, linked under this open K-Mod issue
https://github.com/karadoc/Civ4-K-Mod/issues/1
would be safe for multiplayer, but can't be used in conjunction with the Alt+C keyboard shortcut.

CvGameCoreDLL/CvSelectionGroup.cpp
@@ -22,7 +22,7 @@
 #include "CvDLLPythonIFaceBase.h"
 #include <set>
 #include "CvEventReporter.h"
-
+#include "CvBugOptions.h" // f1rpo (for BUG - Pre-Chop)
 #include "BetterBTSAI.h"
 
 KmodPathFinder CvSelectionGroup::path_finder; // K-Mod
@@ -3528,6 +3528,33 @@ bool CvSelectionGroup::groupBuild(BuildTypes eBuild)
 	}
 	// K-Mod end
 
+	// BUG - Pre-Chop - start  (merged by f1rpo)
+	bool bCheckChop = false;
+	bool bStopOtherWorkers = false;
+
+	FeatureTypes eFeature = pPlot->getFeatureType();
+	CvBuildInfo& kBuildInfo = GC.getBuildInfo(eBuild);
+	if (eFeature != NO_FEATURE && isHuman() && kBuildInfo.isFeatureRemove(eFeature) && kBuildInfo.getFeatureProduction(eFeature) != 0
+		&& !GC.getGame().isNetworkMultiPlayer()) // f1rpo: Would go OOS
+	{
+		if (kBuildInfo.getImprovement() == NO_IMPROVEMENT)
+		{
+			// clearing a forest or jungle
+			if (getBugOptionBOOL("WorkerActions__PreChopForests", true, "BUG_PRECHOP_FORESTS"))
+			{
+				bCheckChop = true;
+			}
+		}
+		else
+		{
+			if (getBugOptionBOOL("WorkerActions__PreChopImprovements", true, "BUG_PRECHOP_IMPROVEMENTS"))
+			{
+				bCheckChop = true;
+			}
+		}
+	}
+	// BUG - Pre-Chop - end
+
 	pUnitNode = headUnitNode();
 
 	while (pUnitNode != NULL)
@@ -3546,8 +3573,45 @@ bool CvSelectionGroup::groupBuild(BuildTypes eBuild)
 				bContinue = false;
 				break;
 			}
+			// BUG - Pre-Chop - start  (merged by f1rpo)
+			if (bCheckChop && pPlot->getBuildTurnsLeft(eBuild, getOwnerINLINE()) == 1)
+			{
+				// TODO: stop other worker groups
+				CvCity* pCity;
+				int iProduction = plot()->getFeatureProduction(eBuild, getTeam(), &pCity);
+
+				if (iProduction > 0)
+				{
+					CvWString szBuffer = gDLL->getText("TXT_KEY_BUG_PRECLEARING_FEATURE_BONUS", GC.getFeatureInfo(eFeature).getTextKeyWide(), iProduction, pCity->getNameKey());
+					gDLL->getInterfaceIFace()->addHumanMessage(getOwnerINLINE(), false, GC.getEVENT_MESSAGE_TIME(), szBuffer,  ARTFILEMGR.getInterfaceArtInfo("WORLDBUILDER_CITY_EDIT")->getPath(), MESSAGE_TYPE_INFO, GC.getFeatureInfo(eFeature).getButton(), (ColorTypes)GC.getInfoTypeForString("COLOR_WHITE"), getX(), getY(), true, true);
+				}
+				bContinue = false;
+				bStopOtherWorkers = true;
+				break;
+			}
+			// BUG - Pre-Chop - end
 		}
 	}
+	// BUG - Pre-Chop - start  (merged by f1rpo)
+	if (bStopOtherWorkers)
+	{
+		pUnitNode = pPlot->headUnitNode();
+
+		while (pUnitNode != NULL)
+		{
+			pLoopUnit = ::getUnit(pUnitNode->m_data);
+			pUnitNode = pPlot->nextUnitNode(pUnitNode);
+			CvSelectionGroup* pSelectionGroup = pLoopUnit->getGroup();
+
+			if (pSelectionGroup != NULL && pSelectionGroup != this && pSelectionGroup->getOwnerINLINE() == getOwnerINLINE()
+					&& pSelectionGroup->getActivityType() == ACTIVITY_MISSION && pSelectionGroup->getLengthMissionQueue() > 0 
+					&& pSelectionGroup->getMissionType(0) == kBuildInfo.getMissionType() && pSelectionGroup->getMissionData1(0) == eBuild)
+			{
+				pSelectionGroup->deleteMissionQueueNode(pSelectionGroup->headMissionQueueNode());
+			}
+		}
+	}
+	// BUG - Pre-Chop - end
 
 	return bContinue;
 }

Assets/Config/BULL Actions.xml
@@ -29,13 +29,6 @@
 			<option id="IgnoreHarmlessBarbarians" key="Ignore Harmless Barbarians" 
 					type="boolean" default="False" dll="2"/>
 		</section>
-		
-		<section id="Workers">
-			<!-- PreChop by EmperorFool -->
-			<option id="PreChopForests" key="Pre Chop Forests" 
-					type="boolean" default="False"/>
-			<option id="PreChopImprovements" key="Pre Chop Improvements" 
-					type="boolean" default="False"/>
-		</section>
+		<!-- f1rpo: Worker actions moved into a separate module -->
 	</options>
 </mod>

Assets/Config/BULL WorkerActions.xml
@@ -1,0 +1,18 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+
+<!-- f1rpo: Moved from the "BULL Actions" module b/c the K-Mod DLL now supports
+	 the worker actions, but still doesn't support the rest. -->
+<mod id="WorkerActions" 
+	 name="BULL WorkerActions" 
+	 author="EmperorFool" 
+	 dll="0">
+
+	<options id="WorkerActions" file="BUG Worker Actions.ini">
+		<section id="PreChop">
+			<option id="PreChopForests" key="Pre Chop Forests" 
+					type="boolean" default="False"/>
+			<option id="PreChopImprovements" key="Pre Chop Improvements" 
+					type="boolean" default="False"/>
+		</section>
+	</options>
+</mod>

Assets/Python/BUG/Tabs/BugGeneralOptionsTab.py
@@ -29,7 +29,8 @@ class BugGeneralOptionsTab(BugOptionsTab.BugOptionsTab):
 
 		#self.createAutoSavePanel(screen, center)
 		self.addSpacer(screen, center, "General3")
-		#self.createActionsPanel(screen, center)
+		# f1rpo: Uncommented -- for Pre-Chop options
+		self.createActionsPanel(screen, center)
 
 		self.createInfoPanePanel(screen, center)
 		self.addSpacer(screen, right, "General4")
@@ -60,11 +61,15 @@ class BugGeneralOptionsTab(BugOptionsTab.BugOptionsTab):
 		
 	def createActionsPanel(self, screen, panel):
 		self.addLabel(screen, panel, "Actions", "Actions:")
-		self.addCheckbox(screen, panel, "Actions__AskDeclareWarUnits")
-		self.addCheckbox(screen, panel, "Actions__SentryHealing")
-		self.addCheckbox(screen, panel, "Actions__SentryHealingOnlyNeutral", True)
-		self.addCheckbox(screen, panel, "Actions__PreChopForests")
-		self.addCheckbox(screen, panel, "Actions__PreChopImprovements")
+		# <f1rpo>
+		# These still aren't implemented
+		#self.addCheckbox(screen, panel, "Actions__AskDeclareWarUnits")
+		#self.addCheckbox(screen, panel, "Actions__SentryHealing")
+		#self.addCheckbox(screen, panel, "Actions__SentryHealingOnlyNeutral", True)
+		# These are now in a separate "WorkerActions" module
+		self.addCheckbox(screen, panel, "WorkerActions__PreChopForests")
+		self.addCheckbox(screen, panel, "WorkerActions__PreChopImprovements")
+		# </f1rpo>
 		
 	def createTechSplashPanel(self, screen, panel):
 		self.addLabel(screen, panel, "TechWindow", "Tech Splash Screen:")

Assets/xml/Text/BULL Actions Options.xml
@@ -80,10 +80,12 @@
 		<Spanish>Si se activa, las unidades bárbaras adyacentes a tu territorio no provocarán una advertencia si no pueden entrar.</Spanish>
 	</TEXT>
 
-<!-- PreChop -->
-
+<!-- PreChop
+	 f1rpo: These options are now in a separate "WorkerActions" module.
+	 "Non-network only" added b/c I'm disabling them in the DLL
+	 for networked multiplayer. (Not for other types of multiplayer.) -->
 	<TEXT>
-		<Tag>TXT_KEY_BUG_OPT_ACTIONS__PRECHOPFORESTS_TEXT</Tag>
+		<Tag>TXT_KEY_BUG_OPT_WORKERACTIONS__PRECHOPFORESTS_TEXT</Tag>
 		<English>PreChop Forests</English>
 		<French>PreChop Forests</French>
 		<German>Warten vor Abholzung</German>
@@ -91,15 +93,15 @@
 		<Spanish>Precortar Bosques</Spanish>
 	</TEXT>
 	<TEXT>
-		<Tag>TXT_KEY_BUG_OPT_ACTIONS__PRECHOPFORESTS_HOVER</Tag>
-		<English>When checked, Workers ordered to chop a forest will stop with 1 turn remaining. Issue the order again to finish the chop.</English>
-		<French>When checked, Workers ordered to chop a forest will stop with 1 turn remaining. Issue the order again to finish the chop.</French>
-		<German>Stoppt Bautrupps, die die Aktion "Wald abholzen" ausführen, 1 Runde, bevor die Aktion beendet wird, und lässt sie auf einen weiteren Befehl warten</German>
-		<Italian>Quando è selezionato, i Lavoratori a cui è stato ordinato di tagliare una Foresta si fermeranno quando mancherà un turno al completamento dell'incarico. Per terminare il disboscamento è necessario impartire nuovamente l'ordine.</Italian>
-		<Spanish>Si se activa, los Trabajadores a los que se les ordene talar un bosque se detendrán cuando les quede un turno para terminar. Si se realiza la orden de nuevo terminarán el talado.</Spanish>
+		<Tag>TXT_KEY_BUG_OPT_WORKERACTIONS__PRECHOPFORESTS_HOVER</Tag>
+		<English>(Non-network games only) When checked, Workers ordered to chop a forest will stop with 1 turn remaining. Issue the order again to finish the chop.</English>
+		<French>(Non-network games only) When checked, Workers ordered to chop a forest will stop with 1 turn remaining. Issue the order again to finish the chop.</French>
+		<German>(Nicht in Netzwerk-Spielen) Stoppt Bautrupps, die die Aktion "Wald abholzen" ausführen, 1 Runde, bevor die Aktion beendet wird, und lässt sie auf einen weiteren Befehl warten</German>
+		<Italian>(Non-network games only) Quando è selezionato, i Lavoratori a cui è stato ordinato di tagliare una Foresta si fermeranno quando mancherà un turno al completamento dell'incarico. Per terminare il disboscamento è necessario impartire nuovamente l'ordine.</Italian>
+		<Spanish>(Non-network games only) Si se activa, los Trabajadores a los que se les ordene talar un bosque se detendrán cuando les quede un turno para terminar. Si se realiza la orden de nuevo terminarán el talado.</Spanish>
 	</TEXT>
 	<TEXT>
-		<Tag>TXT_KEY_BUG_OPT_ACTIONS__PRECHOPIMPROVEMENTS_TEXT</Tag>
+		<Tag>TXT_KEY_BUG_OPT_WORKERACTIONS__PRECHOPIMPROVEMENTS_TEXT</Tag>
 		<English>PreChop Improvements</English>
 		<French>PreChop Improvements</French>
 		<German>Warten vor Fertigstellung</German>
@@ -107,12 +109,12 @@
 		<Spanish>Precortar Mejoras</Spanish>
 	</TEXT>
 	<TEXT>
-		<Tag>TXT_KEY_BUG_OPT_ACTIONS__PRECHOPIMPROVEMENTS_HOVER</Tag>
-		<English>When checked, Workers ordered to build an improvement that will remove a forest will stop with 1 turn remaining. Issue the order again to finish the improvement.</English>
-		<French>When checked, Workers ordered to build an improvement that will remove a forest will stop with 1 turn remaining. Issue the order again to finish the improvement.</French>
-		<German>Stoppt Bautrupps, die eine Modernisierung auf einem Wald-Geländefeld errichten und diesen damit entfernen würden, 1 Runde, bevor die Aktion beendet wird, und lässt sie auf einen weiteren Befehl warten</German>
-		<Italian>Quando è selezionato, i Lavoratori a cui è stato ordinato di tagliare una Foresta e successivamente costruire un miglioramento si fermeranno quando mancherà un turno al completamento dell'incarico. Per terminare la costruzione è necessario impartire nuovamente l'ordine.</Italian>
-		<Spanish>Si se activa, los Trabajadores a los que se les ordene construir una mejora que resulte en el talado de un bosque se detendrán cuando les quede un turno para terminar. Si se realiza la orden de nuevo terminarán la mejora.</Spanish>
+		<Tag>TXT_KEY_BUG_OPT_WORKERACTIONS__PRECHOPIMPROVEMENTS_HOVER</Tag>
+		<English>(Non-network games only) When checked, Workers ordered to build an improvement that will remove a forest will stop with 1 turn remaining. Issue the order again to finish the improvement.</English>
+		<French>(Non-network games only) When checked, Workers ordered to build an improvement that will remove a forest will stop with 1 turn remaining. Issue the order again to finish the improvement.</French>
+		<German>(Nicht in Netzwerk-Spielen) Stoppt Bautrupps, die eine Modernisierung auf einem Wald-Geländefeld errichten und diesen damit entfernen würden, 1 Runde, bevor die Aktion beendet wird, und lässt sie auf einen weiteren Befehl warten</German>
+		<Italian>(Non-network games only) Quando è selezionato, i Lavoratori a cui è stato ordinato di tagliare una Foresta e successivamente costruire un miglioramento si fermeranno quando mancherà un turno al completamento dell'incarico. Per terminare la costruzione è necessario impartire nuovamente l'ordine.</Italian>
+		<Spanish>(Non-network games only) Si se activa, los Trabajadores a los que se les ordene construir una mejora que resulte en el talado de un bosque se detendrán cuando les quede un turno para terminar. Si se realiza la orden de nuevo terminarán la mejora.</Spanish>
 	</TEXT>
 
 </Civ4GameText>

CvGameCoreDLL/CvPlayer.cpp
@@ -9450,6 +9450,36 @@ void CvPlayer::changeWorkerSpeedModifier(int iChange)
 	m_iWorkerSpeedModifier = (m_iWorkerSpeedModifier + iChange);
 }
 
+// BUG - Pre-Chop - start  (merged by f1rpo)
+// Returns the work rate for the first unit that can build <eBuild>.
+int CvPlayer::getWorkRate(BuildTypes eBuild) const
+{
+	int iRate = 0;
+	CvCivilizationInfo& kCiv = GC.getCivilizationInfo(getCivilizationType());
+
+	for (int iI = 0; iI < GC.getNumUnitClassInfos(); iI++)
+	{
+		CvUnitInfo& kUnit = GC.getUnitInfo((UnitTypes)kCiv.getCivilizationUnits(iI));
+
+		if (kUnit.getBuilds(eBuild))
+		{
+			iRate = kUnit.getWorkRate();
+			break;
+		}
+	}
+
+	iRate *= std::max(0, getWorkerSpeedModifier() + 100);
+	iRate /= 100;
+
+	if (!isHuman() && !isBarbarian())
+	{
+		iRate *= std::max(0, (GC.getHandicapInfo(GC.getGameINLINE().getHandicapType()).getAIWorkRateModifier() + 100));
+		iRate /= 100;
+	}
+
+	return iRate;
+}
+// BUG - Pre-Chop - end
 
 int CvPlayer::getImprovementUpgradeRateModifier() const
 {

CvGameCoreDLL/CvPlayer.h
@@ -448,6 +448,7 @@ public:
 
 	int getWorkerSpeedModifier() const;																																		// Exposed to Python
 	void changeWorkerSpeedModifier(int iChange);
+	int getWorkRate(BuildTypes eBuild) const; // BUG - Pre-Chop  (merged by f1rpo)
 
 	int getImprovementUpgradeRateModifier() const;																									// Exposed to Python
 	void changeImprovementUpgradeRateModifier(int iChange);

CvGameCoreDLL/CvPlot.cpp
@@ -2581,6 +2581,78 @@ int CvPlot::getBuildTurnsLeft(BuildTypes eBuild, int iNowExtra, int iThenExtra) 
 	return std::max(1, iTurnsLeft);
 }
 
+// BUG - Pre-Chop - start  (merged by f1rpo)
+int CvPlot::getBuildTurnsLeft(BuildTypes eBuild, PlayerTypes ePlayer) const
+{
+	int iWorkRate = GET_PLAYER(ePlayer).getWorkRate(eBuild);
+	if (iWorkRate > 0)
+	{
+		return getBuildTurnsLeftBUG(eBuild, iWorkRate, iWorkRate, false);
+	}
+	else
+	{
+		return MAX_INT;
+	}
+}
+
+
+int CvPlot::getBuildTurnsLeftBUG(BuildTypes eBuild, int iNowExtra, int iThenExtra, bool bIncludeUnits) const
+{
+	CLLNode<IDInfo>* pUnitNode;
+	CvUnit* pLoopUnit;
+	int iNowBuildRate;
+	int iThenBuildRate;
+	int iBuildLeft;
+	int iTurnsLeft;
+
+	iNowBuildRate = iNowExtra;
+	iThenBuildRate = iThenExtra;
+
+	if (bIncludeUnits)
+	{
+		pUnitNode = headUnitNode();
+
+		while (pUnitNode != NULL)
+		{
+			pLoopUnit = ::getUnit(pUnitNode->m_data);
+			pUnitNode = nextUnitNode(pUnitNode);
+
+			if (pLoopUnit->getBuildType() == eBuild)
+			{
+				if (pLoopUnit->canMove())
+				{
+					iNowBuildRate += pLoopUnit->workRate(false);
+				}
+				iThenBuildRate += pLoopUnit->workRate(true);
+			}
+		}
+	}
+
+	if (iThenBuildRate == 0)
+	{
+		//this means it will take forever under current circumstances
+		return MAX_INT;
+	}
+
+	iBuildLeft = getBuildTime(eBuild);
+
+	iBuildLeft -= getBuildProgress(eBuild);
+	iBuildLeft -= iNowBuildRate;
+
+	iBuildLeft = std::max(0, iBuildLeft);
+
+	iTurnsLeft = (iBuildLeft / iThenBuildRate);
+
+	if ((iTurnsLeft * iThenBuildRate) < iBuildLeft)
+	{
+		iTurnsLeft++;
+	}
+
+	iTurnsLeft++;
+
+	return std::max(1, iTurnsLeft);
+}
+// BUG - Pre-Chop - end
 
 int CvPlot::getFeatureProduction(BuildTypes eBuild, TeamTypes eTeam, CvCity** ppCity) const
 {

AdrianGin/AdvCivX

commit 887d68b9067e3796200cfbe3185423a5d7bc16fa

More reliable can-bombard checks in AI code
Cherry picked from commit 8d14920f7dddfdbc18110ed8238c70b398b0231c.

Also fixes inconsistent line ending introduced by commit 53fb260a7892f0ac05fb72fd8d5db87e3670aa6b.

CvGameCoreDLL/CvSelectionGroup.cpp
@@ -2591,6 +2591,10 @@ bool CvSelectionGroup::canDefend()
 
 bool CvSelectionGroup::canBombard(const CvPlot* pPlot)
 {
+	/*	f1rpo (note): For AI units, it's usually enough to check the head
+		unit - due to the group ordering imposed by addUnit. There are exceptions
+		though (sea units in particular). Will have to leave it up to callers
+		to decide whether they need fast or reliable results. */
 	CLLNode<IDInfo>* pUnitNode = headUnitNode();
 	while (pUnitNode != NULL)
 	{

CvGameCoreDLL/CvUnitAI.cpp
@@ -3056,7 +3056,7 @@ void CvUnitAI::AI_attackCityMove()
 					if (AI_omniGroup(UNITAI_ATTACK_CITY, -1, -1, true, iMoveFlags, 3, true, false, bIgnoreFaster)) // bigger groups only
 						return;
 
-					if (canBombard(plot()))
+					if (getGroup()->canBombard(plot())) // f1rpo: check group (advc.001j)
 					{
 						getGroup()->pushMission(MISSION_BOMBARD, -1, -1, 0, false, false, MISSIONAI_ASSAULT, pTargetCity->plot());
 						return;
@@ -15491,7 +15491,7 @@ bool CvUnitAI::AI_bombardCity()
 		}
 	}
 
-	if (!canBombard(plot()))
+	if (!getGroup()->canBombard(plot())) // f1rpo: check group (advc.001j)
 		return false;
 
 	CvCity* pBombardCity = bombardTarget(plot());
@@ -16127,7 +16127,7 @@ bool CvUnitAI::AI_blockade()
 
 										iValue += (GET_PLAYER(getOwnerINLINE()).AI_plotTargetMissionAIs(pCity->plot(), MISSIONAI_ASSAULT, getGroup(), 2) * 3);
 
-										if (canBombard(pLoopPlot))
+										if (getGroup()->canBombard(pLoopPlot)) // f1rpo: check group (advc.001j)
 										{
 											iValue *= 2;
 										}
@@ -16176,7 +16176,7 @@ bool CvUnitAI::AI_blockade()
 
 		if (atPlot(pBestBlockadePlot))
 		{
-			if (canBombard(plot()))
+			if (getGroup()->canBombard(plot())) // f1rpo: check group (advc.001j)
 			{
 				getGroup()->pushMission(MISSION_BOMBARD, -1, -1, 0, false, false, MISSIONAI_BLOCKADE, pBestBlockadePlot);
 			}
@@ -23652,7 +23652,7 @@ bool CvUnitAI::AI_followBombard()
 	CvPlot* pAdjacentPlot2;
 	int iI, iJ; */ // K-mod disabled
 
-	if (canBombard(plot()))
+	if (getGroup()->canBombard(plot())) // f1rpo: check group (advc.001j)
 	{
 		getGroup()->pushMission(MISSION_BOMBARD);
 		return true;